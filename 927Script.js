140d4072eefd1b40691cfaea21d6ea664168dafb
MSL <32114609+Michael-St-Louis@users.noreply.github.com>
2017-09-27 19:46:40 -0400

    Create 927Script.js

---
+++ 927Script.js
@@ -0,0 +1,9 @@
+window.alert("Press the buttons to change fonts!")
+
+function comicSans(){
+document.getElementById("font").style.fontFamily="Comic Sans Ms";
+}
+
+function wingdings(){
+document.getElementById("font").style.fontFamily="Wingdings";
+}
